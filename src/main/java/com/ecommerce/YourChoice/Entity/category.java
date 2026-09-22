bb2c25067909b85e47ec93250f98bc1afe16d9ac
ArunDharani <myimportantworks2@gmail.com>
2026-09-22 08:10:44 +0530

    Added first line of validation

--- src/main/java/com/ecommerce/YourChoice/Entity/category.java
+++ src/main/java/com/ecommerce/YourChoice/Entity/category.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 
@@ -17,6 +18,8 @@ public class category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotEmpty(message = "Category cannot be empty")
     private String categoryName;
 
 }
