32bba852194cb17526102d2a820e18fdd468e4a1
ArunDharani <myimportantworks2@gmail.com>
2026-09-22 12:16:23 +0530

    Created a Global Custom Method Arguemnt Exception

--- src/main/java/com/ecommerce/YourChoice/Exception/myGlobalException.java
+++ src/main/java/com/ecommerce/YourChoice/Exception/myGlobalException.java
@@ -1,12 +1,24 @@
 package com.ecommerce.YourChoice.Exception;
-
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
-public class myGlobalException extends RuntimeException {
-    public myGlobalException(String message) {
-        super(message);
+public class myGlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String , String>> myInvalidArgExcp(MethodArgumentNotValidException input) {
+        Map<String , String> response = new HashMap<>();
+        input.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String fieldMessage = err.getDefaultMessage();
+            response.put(fieldName , fieldMessage);
+        });
+        return new ResponseEntity<Map<String , String>>(response , HttpStatus.BAD_REQUEST);
     }
 }
